3a6088accb31d8f281b4675134e130dbaf6145d5
Guizinduarrancadiu <115833117+GuizinChucro@users.noreply.github.com>
2023-09-26 21:59:31 -0300

    commit 03

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -20,3 +20,15 @@ BEGIN
     INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
     WHERE Categoria.Nome = categoria_nome;
 END;
+
+3. Contagem de Livros por Categoria:
+
+sql
+DELIMITER //
+
+CREATE PROCEDURE sp_ContarLivrosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    SELECT COUNT(Livro.Livro_ID) AS total
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    
