a9dc32295f724025aeb39135238f954130bf579c
Guizinduarrancadiu <115833117+GuizinChucro@users.noreply.github.com>
2023-09-26 21:58:29 -0300

    cimmit 02

--- listaExercicios02.sql
+++ listaExercicios02.sql
@@ -7,3 +7,16 @@ CREATE PROCEDURE sp_ListarAutores()
 BEGIN
     SELECT * FROM Autor;
 END;
+
+2. Livros por Categoria:
+
+sql
+DELIMITER //
+
+CREATE PROCEDURE sp_LivrosPorCategoria(IN categoria_nome VARCHAR(100))
+BEGIN
+    SELECT Livro.Titulo
+    FROM Livro
+    INNER JOIN Categoria ON Livro.Categoria_ID = Categoria.Categoria_ID
+    WHERE Categoria.Nome = categoria_nome;
+END;
